2d40d7864033e8bde68839a8ce27b01a56d3cd42
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2023-10-11 18:38:12 +0200

    added Send SMS with Template

--- includes/excitesms-api.php
+++ includes/excitesms-api.php
@@ -1,38 +1,48 @@
 <?php
-function send_sms_using_excitesms($recipient, $message) {
+function send_sms_using_excitesms_with_template($recipient, $template_name, $template_variables) {
     $api_key = get_option('excitesms_api_key');
     $sender_id = get_option('excitesms_sender_id');
 
     if (empty($api_key) || empty($sender_id)) {
-        // Handle missing API key or sender ID
         return new WP_Error('excitesms_missing_settings', 'API Key or Sender ID is missing.');
     }
 
-    $url = 'https://portal.excitesms.tech/api/v3/sms/send';
+    $templates = get_sms_templates();
 
-    $data = array(
-        'recipient' => $recipient,
-        'sender_id' => $sender_id,
-        'type' => 'plain',
-        'message' => $message
-    );
+    if (isset($templates[$template_name])) {
+        $template_content = $templates[$template_name];
 
-    $headers = array(
-        'Authorization: Bearer ' . $api_key,
-        'Content-Type: application/json',
-        'Accept: application/json'
-    );
+        // Replace template variables with actual data
+        foreach ($template_variables as $variable => $value) {
+            $template_content = str_replace("{{$variable}}", $value, $template_content);
+        }
 
-    $response = wp_safe_remote_post($url, array(
-        'headers' => $headers,
-        'body' => json_encode($data)
-    ));
+        $url = 'https://portal.excitesms.tech/api/v3/sms/send';
 
-    if (is_wp_error($response)) {
-        // Handle API request error
-        return $response;
+        $data = array(
+            'recipient' => $recipient,
+            'sender_id' => $sender_id,
+            'type' => 'plain',
+            'message' => $template_content
+        );
+
+        $headers = array(
+            'Authorization: Bearer ' . $api_key,
+            'Content-Type: application/json',
+            'Accept: application/json'
+        );
+
+        $response = wp_safe_remote_post($url, array(
+            'headers' => $headers,
+            'body' => json_encode($data)
+        ));
+
+        if (is_wp_error($response)) {
+            return $response;
+        } else {
+            return wp_remote_retrieve_body($response);
+        }
     } else {
-        // Handle success
-        return wp_remote_retrieve_body($response);
+        return new WP_Error('excitesms_template_not_found', 'Template not found.');
     }
 }
